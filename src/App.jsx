95b8b9466ecf8093239cfbf0952479eca9125ab6
Melkor_1 <127069062+WWI2196@users.noreply.github.com>
2025-04-11 16:18:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <main>
+      <Hero className="relative min-h-screen w-screen overflow-x-hidden"/>
+    </main>
+  )
+}
+
+export default App
